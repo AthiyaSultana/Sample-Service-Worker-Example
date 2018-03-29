6c6b384702b8d05bc7c77d3f514133daca0d31ea
Athiya Sultana <athiya@olivetech.net>
2018-03-29 17:45:10 +0530

    Added manifest file and modified service worker fetch event

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-var cacheName = 'cache5';
+var cacheName = 'cache8';
+var dataCacheName = 'weatherData-v1';
 var filesToCache = [
     '/',
     '/index.html',
@@ -21,8 +22,7 @@ var filesToCache = [
 console.log('self', self);
 console.log('this', this);
 self.addEventListener('install', function (event) {
-    self.skipWaiting().then(function () {
-
+    this.skipWaiting().then(function () {
     });
     event.waitUntil(
         caches.open(cacheName).then(function (cache) {
@@ -30,6 +30,7 @@ self.addEventListener('install', function (event) {
             return cache.addAll(filesToCache);
         })
     )
+    //return this.clients.claim();
 });
 
 self.addEventListener('activate', function (event) {
@@ -37,21 +38,44 @@ self.addEventListener('activate', function (event) {
         caches.keys().then(function (keyList) {
             console.log('keyList', keyList);
             return Promise.all(keyList.map(function (key) {
-                if (key !== cacheName) {
+                if (key !== cacheName && key !== dataCacheName) {
                     console.log('[ServiceWorker] Removing old cache', key);
                     return caches.delete(key);
                 }
             }))
         })
     )
-    return self.clients.claim();
 });
 
-self.addEventListener('fetch', function (event) {
-    console.log('[ServiceWorker] Fetch', event.request.url);
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
-    )
+self.addEventListener('fetch', function (e) {
+    console.log('[Service Worker] Fetch', e.request.url);
+    var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+    if (e.request.url.indexOf(dataUrl) > -1) {
+        /*
+         * When the request URL contains dataUrl, the app is asking for fresh
+         * weather data. In this case, the service worker always goes to the
+         * network and then caches the response. This is called the "Cache then
+         * network" strategy:
+         * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+         */
+        e.respondWith(
+            caches.open(dataCacheName).then(function (cache) {
+                return fetch(e.request).then(function (response) {
+                    cache.put(e.request.url, response.clone());
+                    return response;
+                });
+            })
+        );
+    } else {
+        /*
+         * The app is asking for app shell files. In this scenario the app uses the
+         * "Cache, falling back to the network" offline strategy:
+         * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+         */
+        e.respondWith(
+            caches.match(e.request).then(function (response) {
+                return response || fetch(e.request);
+            })
+        );
+    }
 });
